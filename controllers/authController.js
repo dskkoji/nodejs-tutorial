fa46017f06cd870549ca16ec69ebe1188d7e0bf4
dskkoji <dkojima.2nd@gmail.com>
2023-11-17 10:41:11 +0900

    modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,20 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require('../model/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const handleLogin = async (req, res) => {
-    const cookies = req.cookies;
-    console.log(`cookie available at login: ${JSON.stringify(cookies)}`);
-    const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+    const cookies = req.cookies
+    console.log(`cookie available at login: ${JSON.stringify(cookies)}`)
+    const { user, pwd } = req.body
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and Password are required'})
 
-    const foundUser = await User.findOne({ username: user }).exec();
-    if (!foundUser) return res.sendStatus(401); //Unauthorized 
-    // evaluate password 
-    const match = await bcrypt.compare(pwd, foundUser.password);
+    const foundUser = await User.findOne({ username: user }).exec()
+    if (!foundUser) return res.sendStatus(401) // Unauthorized
+    // evaluate password
+    const match = await bcrypt.compare(pwd, foundUser.password)
     if (match) {
-        const roles = Object.values(foundUser.roles).filter(Boolean);
-        // create JWTs
+        const roles = Object.values(foundUser.roles).filter(Boolean)
+        // Create JWTs
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
@@ -24,55 +24,48 @@ const handleLogin = async (req, res) => {
             },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '10s' }
-        );
+        )
+
         const newRefreshToken = jwt.sign(
             { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
-        );
+        )
 
         // Changed to let keyword
-        let newRefreshTokenArray =
-            !cookies?.jwt
+        let newRefreshTokenArray = 
+            !cookies.jwt
                 ? foundUser.refreshToken
-                : foundUser.refreshToken.filter(rt => rt !== cookies.jwt);
+                : foundUser.refreshToken.filter(rt => rt !== cookies.jwt)
+        if (cookies.jwt) {
 
-        if (cookies?.jwt) {
+            const refreshToken = cookies.jwt
+            const foundToken = await User.findOne({ refreshToken }).exec()
 
-            /* 
-            Scenario added here: 
-                1) User logs in but never uses RT and does not logout 
-                2) RT is stolen
-                3) If 1 & 2, reuse detection is needed to clear all RTs when user logs in
-            */
-            const refreshToken = cookies.jwt;
-            const foundToken = await User.findOne({ refreshToken }).exec();
-
-            // Detected refresh token reuse!
+            // Detected refresh token reuse
             if (!foundToken) {
                 console.log('attempted refresh token reuse at login!')
-                // clear out ALL previous refresh tokens
-                newRefreshTokenArray = [];
+                // clear out ALl previous refresh token
+                newRefreshTokenArray = []
             }
 
-            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+            res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
         }
 
         // Saving refreshToken with current user
-        foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
-        const result = await foundUser.save();
-        console.log(result);
-        console.log(roles);
+        foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken]
+        const result = await foundUser.save()
+        console.log(result)
+        console.log(roles)
 
         // Creates Secure Cookie with refresh token
-        res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+        res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 })
 
         // Send authorization roles and access token to user
-        res.json({ roles, accessToken });
-
+        res.json({ roles, accessToken })
     } else {
-        res.sendStatus(401);
+        res.sendStatus(401)
     }
 }
 
-module.exports = { handleLogin };
+module.exports = { handleLogin }
